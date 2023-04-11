52ea09a15cbac37724ceac150f1a6a69d7a69602
Martin Apiwat <martin.apiwat@gmail.com>
2023-04-11 13:13:53 +0200

    added my coding languages

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,28 @@ import "./App.scss";
 export default function App() {
    return (
       <div>
-         <div className="main">
-            <div className="welcome">
-               <h1>Welcome to my portfolio</h1>
+         <div className="navbar">
+            <ul>
+               <li>About me</li>
+               <li>Projects</li>
+               <li>Contact</li>
+            </ul>
+         </div>
+         <div className="welcome">
+            <h1>Welcome to my portfolio</h1>
+         </div>
+         <div className="image-text">
+            <img src="./img/Martin.jpeg" alt="" height="400px" />
+            <div className="about-me">
+               <p>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem
+                  sunt excepturi dolor odio veniam ad omnis non suscipit quas
+                  itaque.
+               </p>
             </div>
-            <div className="image-text">
-               <img src="./img/Martin.jpeg" alt="" height="500px" />
-               <div className="about-text">
-                  <p>
-                     Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                     Rem sunt excepturi dolor odio veniam ad omnis non suscipit
-                     quas itaque.
-                  </p>
-               </div>
-            </div>
+         </div>
+         <div className="languages">
+            <h3>Typescript, React, Next, Node, Express, MongoDB, Sass, CSS</h3>
          </div>
       </div>
    );
